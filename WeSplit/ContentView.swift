c2b3227ce52c65ad3926ed12acc93bdf6a2b7e16
sxmtee <somtoo76@gmail.com>
2024-04-01 23:57:34 +0100

    All changes

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercntages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            Form{
+                Section{
+                    TextField(
+                        "Amount",
+                        value: $checkAmount,
+                        format: .currency(code: Locale.current.currency?.identifier ?? "CAD")
+                    )
+                    .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) { number in
+                            Text("\(number) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section("How much do you want to tip?") {
+                    Picker("Tip Percentage", selection: $tipPercentage){
+                        ForEach(tipPercntages, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section{
+                    Text(
+                        totalPerPerson,
+                        format: .currency(code: Locale.current.currency?.identifier ?? "CAD")
+                    )
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar{
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
--- WeSplit/WeSplitAssignment.swift
+++ WeSplit/WeSplitAssignment.swift
@@ -5,4 +5,102 @@
 //  Created by mac on 30/03/2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct WeSplitAssignment: View {
+    @State private var inputValue = ""
+    @State private var selectedInputUnit = 0
+    @State private var selectedOutputUnit = 2
+    @FocusState private var amountIsFocused: Bool
+        
+    let units = ["Liters", "Milliliters", "Cups", "Pints"]
+    
+    var convertedValue: Double {
+        let input = Double(inputValue) ?? 0
+        let inputInMilliliters = convertToMilliliters(value: input, from: units[selectedInputUnit])
+        return convertFromMilliliters(value: inputInMilliliters, to: units[selectedOutputUnit])
+    }
+       
+    func convertToMilliliters(value: Double, from unit: String) -> Double {
+        switch unit {
+            case "Liters":
+                return value * 1000
+            case "Milliliters":
+                return value
+            case "Cups":
+                return value * 236.588
+            case "Pints":
+                return value * 473.176
+            default:
+                return 0
+        }
+    }
+       
+    func convertFromMilliliters(value: Double, to unit: String) -> Double {
+        switch unit {
+            case "Liters":
+                return value / 1000
+            case "Milliliters":
+                return value
+            case "Cups":
+                return value / 236.588
+            case "Pints":
+                return value / 473.176
+            default:
+                return 0
+        }
+    }
+        
+    var body: some View{
+        NavigationStack{
+            Form{
+                Section("Enter Input Value") {
+                    TextField(
+                        "Enter Value",
+                        text: $inputValue
+                    )
+                    .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
+                }
+                
+                Section("Enter Input Unit") {
+                    Picker("Select unit", selection: $selectedInputUnit) {
+                        ForEach(0 ..< units.count, id: \.self) { index in
+                            Text("\(self.units[index])")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("Enter Output Unit") {
+                    Picker("Select unit", selection: $selectedOutputUnit) {
+                        ForEach(0 ..< units.count, id: \.self) {
+                            Text("\(self.units[$0])")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("Output Value") {
+                    Text(
+                        convertedValue,
+                        format: .number
+                    )
+                }
+            }
+            .navigationTitle("Unit Converter")
+            .toolbar{
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+#Preview {
+    WeSplitAssignment()
+}
